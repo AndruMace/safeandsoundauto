6a816184e0cea371c7fd8f6fe9658d39dcefbb7f
Andru Mace <amacedeveloper@gmail.com>
2024-02-16 19:48:34 -0500

    init commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+console.log("Test1")
+document.querySelectorAll('.service-item').forEach(item => {
+  console.log("Test2")
+  let currentIndex = 0;
+  const images = item.querySelectorAll('.carousel-image');
+  const nextButton = item.querySelector('.next');
+  const prevButton = item.querySelector('.prev');
+  function showImage(index) {
+    images.forEach(img => img.classList.remove('active'));
+    images[index].classList.add('active');
+  }
+
+  nextButton.addEventListener('click', () => {
+    console.log('next')
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  });
+
+  prevButton.addEventListener('click', () => {
+    console.log('[rev]')
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+  });
+
+  showImage(currentIndex);
+});
